2bcc3e932ce59c367a5c62a7dad920433e1fcb40
Ellie04162606 <1181140808@qq.com>
2020-08-27 15:07:41 +0800

    feature : Extraction select type  method

---
+++ src/statement.js
@@ -0,0 +1,51 @@
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    thisAmount = selectType(play,perf);
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    //print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits \n`;
+  return result;
+}
+
+function selectType(play,perf){
+let amount=0;
+ switch (play.type) {
+      case 'tragedy':
+        amount = 40000;
+        if (perf.audience > 30) {
+          amount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        amount = 30000;
+        amount += 300 * perf.audience;
+        if (perf.audience > 20) {
+          amount += 10000 + 500 * (perf.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return amount;
+}
+
+module.exports = {
+  statement,
+};
